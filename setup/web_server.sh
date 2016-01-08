238a9a45d23621c4b0c8f772ae76cd2e78581803
Patrick Murray <patrickm129@gmail.com>
2016-01-07 20:05:19 -0500

    Resolving adduser ambiguity

--- setup/web_server.sh
+++ setup/web_server.sh
@@ -15,7 +15,7 @@ apt-get install php5-mcrypt -y
 apt-get install git -y
 
 # Add User
-adduser automatafiddle -d /var/www -G www-data
+adduser automatafiddle --home /var/www --ingroup www-data
 chgrp -R www-data /var/www
 chmod -R g+w /var/www
 chmod g+s /var/www
