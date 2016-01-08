0579c2a03498c72fd8342880202e47747631a2e7
Patrick Murray <patrickm129@gmail.com>
2016-01-08 10:24:37 -0500

    More web_server.sh testing

--- setup/web_server.sh
+++ setup/web_server.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/env bash
+
+USER_NAME = "automatafiddle"
+USER_HOME = "/var/www"
+APCH_GRUP = "www-data"
+
 apt-get update
 apt-get upgrade -y
 
+
 # Apache
 apt-get install apache2 -y
+
 
 # PHP
 apt-get install php5 -y
@@ -11,22 +18,34 @@ apt-get install libapache2-mod-php5 -y
 apt-get install php5-mysql -y
 apt-get install php5-mcrypt -y
 
+
 # Install Git
 apt-get install git -y
 
+
 # Add User
-adduser automatafiddle --home /var/www --ingroup www-data
-chgrp -R www-data /var/www
-chmod -R g+w /var/www
-chmod g+s /var/www
+adduser $USER_NAME --home $USER_HOME --ingroup $APCH_GRUP
+chgrp -R $APCH_GRUP $USER_HOME
+chmod -R g+w $USER_HOME
+chmod g+s $USER_HOME
+
 
 # Setup Git
-su automatafiddle -c "if [ -d '~/AutomataFiddle' ] ; then rm -rf ~/AutomataFiddle ; fi"
-su automatafiddle -c "cd ~; git clone https://github.com/PatrickMurray/AutomataFiddle.git"
+cd $USER_HOME;
+if [ -d AutomataFiddle ] ; then
+	su automatafiddle -c "rm -rf AutomataFiddle"
+fi
+su automatafiddle -c "git clone https://github.com/PatrickMurray/AutomataFiddle.git"
+cd ~;
+
 
 # Setup Configurations
 if [ -f /etc/apache2/sites-enabled/000-default.conf ] ; then
 	rm /etc/apache2/sites-enabled/000-default.conf;
+fi
+
+if [ -f /etc/apache2/sites-enabled/automatafiddle.conf ] ; then
+	rm /etc/apache2/sites-enabled/automatafiddle.conf;
 fi
 
 ln -s /var/www/AutomataFiddle/config/apache2/automatafiddle.conf /etc/apache2/sites-enabled/automatafiddle.conf
