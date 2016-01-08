2d05e129dc586a7cae82d2df967fcb940304c4f3
Patrick Murray <patrickm129@gmail.com>
2016-01-08 15:48:06 -0500

    Testing deployment w/ webhooks

--- src/api/index.php
+++ src/api/index.php
@@ -1,3 +1,9 @@
 <?php
 
-print "Hello World";
+if ($_SERVER["REQUEST_URI"] === "/webhook")
+{
+	$GIT_PATH = "/var/www/AutomataFiddle";
+	shell_exec("cd {$GIT_PATH}; git pull origin master; service apache2 reload;");
+}
+
+print "Hello, World!";
