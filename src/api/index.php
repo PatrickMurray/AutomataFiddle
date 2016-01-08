852d1f3e05050b533752d5137356b79db91f869c
Patrick Murray <patrickm129@gmail.com>
2016-01-08 16:03:33 -0500

    Printing shell output

--- src/api/index.php
+++ src/api/index.php
@@ -1,12 +1,7 @@
 <?php
 
-print $_SERVER["REQUEST_URI"];
-
 if ($_SERVER["REQUEST_URI"] === "/webhook")
 {
 	$GIT_PATH = "/var/www/AutomataFiddle";
-	shell_exec("cd " . $GIT_PATH ."; git pull origin master; service apache2 reload;");
-	print "Hello Git!";
+	print shell_exec("cd " . $GIT_PATH ."; git pull origin master; service apache2 reload;");
 }
-
-print "Hello, World!";
