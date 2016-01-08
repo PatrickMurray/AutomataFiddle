fca475fd5a09fec7abd4a27e859a8af10abe9112
Patrick Murray <patrickm129@gmail.com>
2016-01-08 16:00:47 -0500

    Added prints for debugging

--- src/api/index.php
+++ src/api/index.php
@@ -1,9 +1,12 @@
 <?php
 
+print $_SERVER["REQUEST_URI"];
+
 if ($_SERVER["REQUEST_URI"] === "/webhook")
 {
 	$GIT_PATH = "/var/www/AutomataFiddle";
-	shell_exec("cd $GIT_PATH; git pull origin master; service apache2 reload;");
+	shell_exec("cd " . $GIT_PATH ."; git pull origin master; service apache2 reload;");
+	print "Hello Git!";
 }
 
 print "Hello, World!";
