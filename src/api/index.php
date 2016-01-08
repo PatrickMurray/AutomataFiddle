ff75516aac32a3993c4320f7e5c605dbb767a820
Patrick Murray <patrickm129@gmail.com>
2016-01-08 15:51:13 -0500

    Continued webhook testing

--- src/api/index.php
+++ src/api/index.php
@@ -3,7 +3,7 @@
 if ($_SERVER["REQUEST_URI"] === "/webhook")
 {
 	$GIT_PATH = "/var/www/AutomataFiddle";
-	shell_exec("cd {$GIT_PATH}; git pull origin master; service apache2 reload;");
+	shell_exec("cd $GIT_PATH; git pull origin master; service apache2 reload;");
 }
 
 print "Hello, World!";
