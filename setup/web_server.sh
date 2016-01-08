fa3e560a3acfb559e1be0b647a7bd2219d4a42d3
Patrick Murray <patrickm129@gmail.com>
2016-01-07 19:34:39 -0500

    Created a default Apache2 configuration file and pushed progress to the web server setup script.

---
+++ setup/web_server.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/bash
+apt-get update
+apt-get upgrade -y
+
+# Apache
+apt-get install apache2 -y
+
+# PHP
+apt-get install php5 -y
+apt-get install libapache2-mod-php5 -y
+apt-get install php5-mysql -y
+apt-get install php5-mcrypt -y
+
+# Install Git
+apt-get install git -y
+
+# Add User
+adduser automatafiddle
+
+# Setup Git
+su automatafiddle -c "cd ~; git clone https://github.com/PatrickMurray/AutomataFiddle.git"
+
+# Setup Configurations
+rm /etc/apache2/sites-enabled/000-default.conf
+ln -s /home/automatafiddle/AutomataFiddle/config/apache2/automatafiddle.conf /etc/apache2/sites-enabled/automatafiddle.conf
+
+service restart apache2
