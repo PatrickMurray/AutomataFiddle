9b07673c2de729afb213c7f697f39ed2a78a93b7
Patrick Murray <patrickm129@gmail.com>
2016-01-10 15:26:39 -0500

    Added the GraphViz package to the install list

--- setup/web_server.sh
+++ setup/web_server.sh
@@ -13,8 +13,8 @@ fi
 apt-get update
 apt-get upgrade -y
 
-# Install Fail2Ban, Apache, PHP, MySQL Interface, and Git
-apt-get install sudo fail2ban apache2 php5 libapache2-mod-php5 php5-mysql php5-mcrypt git -y
+# Install Fail2Ban, Apache, PHP, MySQL Interface, Git, and GraphViz
+apt-get install sudo fail2ban apache2 php5 libapache2-mod-php5 php5-mysql php5-mcrypt git graphviz -y
 
 
 # Grant www-data permission to use Git and modify the Apache service
