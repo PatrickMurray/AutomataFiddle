04f1e142d8c7263da8a1141092b150370ca69c82
Patrick Murray <patrickm129@gmail.com>
2016-01-10 13:10:48 -0500

    Turns out the www-data user does not need to be created to execute commands as it

--- setup/web_server.sh
+++ setup/web_server.sh
@@ -10,19 +10,12 @@ apt-get upgrade -y
 # Install Apache, PHP, and Git
 apt-get install apache2 php5 libapache2-mod-php5 php5-mysql php5-mcrypt git -y
 
-# Add User
-adduser $USER_NAME --home $USER_HOME --ingroup $APCH_GRUP
-chgrp -R $APCH_GRUP $USER_HOME
-chmod -R g+w $USER_HOME
-chmod g+s $USER_HOME
-
-
 # Setup Git
 cd $USER_HOME;
 if [ -d $USER_NAME ] ; then
-	su $USER_NAME -c "rm -rf AutomataFiddle"
+	su - $USER_NAME -c "rm -rf AutomataFiddle"
 fi
-su $USER_NAME -c "git clone https://github.com/PatrickMurray/AutomataFiddle.git"
+su - $USER_NAME -c "git clone https://github.com/PatrickMurray/AutomataFiddle.git"
 cd ~;
 
 # Configure Apache
@@ -41,8 +34,8 @@ if [ -f /etc/apache2/sites-enabled/automatafiddle.conf ] ; then
 	rm /etc/apache2/sites-enabled/automatafiddle.conf;
 fi
 
-ln -s /var/www/AutomataFiddle/config/apache2/apache2.conf /etc/apache2/apache2.conf
-ln -s /var/www/AutomataFiddle/config/apache2/automatafiddle.conf /etc/apache2/sites-enabled/automatafiddle.conf
+ln -s $USER_HOME/AutomataFiddle/config/apache2/apache2.conf /etc/apache2/apache2.conf
+ln -s $USER_HOME/AutomataFiddle/config/apache2/automatafiddle.conf /etc/apache2/sites-enabled/automatafiddle.conf
 
 a2enmod rewrite
 
