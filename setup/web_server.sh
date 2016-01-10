4387e198e299742099b54e0be510e30a97888ba4
Patrick Murray <patrickm129@gmail.com>
2016-01-10 14:05:17 -0500

    Granting www-data permissions to /var/www

--- setup/web_server.sh
+++ setup/web_server.sh
@@ -12,6 +12,11 @@ apt-get install sudo apache2 php5 libapache2-mod-php5 php5-mysql php5-mcrypt git
 
 # Setup Git
 echo "git    ALL = (www-data) /usr/bin/git pull" >> /etc/sudoers
+
+chgrp -R $APCH_GRUP $USER_HOME
+chmod -R g+w $USER_HOME
+chmod g+s $USER_HOME
+
 cd $USER_HOME;
 if [ -d $USER_NAME ] ; then
 	sudo -u $USER_NAME "rm -rf AutomataFiddle"
