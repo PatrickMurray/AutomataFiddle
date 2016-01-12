beb86b943df9bdf3fefedab9fa95bfabce165621
Patrick Murray <patrickm129@gmail.com>
2016-01-12 12:22:06 -0500

    Fixing syntax error in index.php

--- src/api/index.php
+++ src/api/index.php
@@ -2,8 +2,8 @@
 
 if ($_SERVER["REQUEST_URI"] === "/webhook")
 {
-	exec("git pull origin master",          NULL, $git_code);
-	exec("sudo /etc/init.d/apache2 reload", NULL, $apache_code);
+	exec("git pull origin master",          $null, $git_code);
+	exec("sudo /etc/init.d/apache2 reload", $null, $apache_code);
 	
 	print "Git: " . $git_code . "\n";
 	print "Apache: " . $apache_code . "\n";
