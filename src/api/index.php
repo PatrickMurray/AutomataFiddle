63507ff7af53c197fd3015aa2bd6a3335e986169
Patrick Murray <patrickm129@gmail.com>
2016-01-12 12:18:25 -0500

    Refactored webhooks to look at the status code of git and apache to verify they completed successfully

--- src/api/index.php
+++ src/api/index.php
@@ -2,9 +2,13 @@
 
 if ($_SERVER["REQUEST_URI"] === "/webhook")
 {
-	print shell_exec("git pull origin master; sudo /etc/init.d/apache2 reload;");
+	exec("git pull origin master",          NULL, $git_code);
+	exec("sudo /etc/init.d/apache2 reload", NULL, $apache_code);
+	
+	print "Git: " . $git_code . "\n";
+	print "Apache: " . $apache_code . "\n";
 }
 else
 {
-	print http_get_request_body();
+	print "Body:\n" . http_get_request_body();
 }
