c0d880480a28d9c10104755462a146e0666a3469
Patrick Murray <patrickm129@gmail.com>
2016-01-21 14:54:31 -0500

    Hard coded in an example render request

--- src/api/index.php
+++ src/api/index.php
@@ -21,27 +21,62 @@ if ($_SERVER["REQUEST_URI"] === "/webhook")
 	if ($git_code < 0 || $apache_code < 0)
 	{
 		header("HTTP/1.1 500 Internal Server Error");
-		trigger_error("Webhook Error: git returned " . $git_code . ", apache returned " . $apache_code);
+		trigger_error("Webhook Error: git={$git_code}, apache={$apache_code}");
 		exit(1);
 	}
 	
 	header("HTTP/1.1 202 Accepted");
 	exit(0);
 }
-else if ($_SERVER["REQUEST_URI"] == "/supported")
+else if ($_SERVER["REQUEST_URI"] === "/supported")
 {
 	$supported = array(
 		"directions" => $GRAPH_DIRECTIONS,
 		"formats"    => $GRAPH_EXPORT_FORMATS,
 		"shapes"     => $GRAPH_NODE_SHAPES
 	);
-
+	
 	print(json_encode($supported));
 	exit(0);
 }
-else
+else if ($_SERVER["REQUEST_URI"] === "/render")
 {
+	/*
 	$body = http_get_request_body();
 	$data = json_decode($body);
 	print_r($data);
+	*/
+	
+	$graph = new Graph();
+	
+	$graph->set_direction("LR");
+	$graph->set_export_format("svg");
+	
+	$graph->add_node("");
+	$graph->add_node("Q0");
+	$graph->add_node("Q1");
+	$graph->add_node("Q2");
+	$graph->add_node("Q3");
+	
+	$graph->set_node_shape("", "none");
+	$graph->set_node_shape("Q0", "circle");
+	$graph->set_node_shape("Q1", "doublecircle");
+	$graph->set_node_shape("Q2", "doublecircle");
+	$graph->set_node_shape("Q3", "circle");
+
+	$graph->add_edge("", "Q0");
+	$graph->add_edge("Q0", "Q1", "A");
+	$graph->add_edge("Q0", "Q3", "B");
+	$graph->add_edge("Q1", "Q1", "A");
+	$graph->add_edge("Q1", "Q2", "B");
+	$graph->add_edge("Q2", "Q2", "A");
+	$graph->add_edge("Q2", "Q3", "B");
+	$graph->add_edge("Q3", "Q3", "A, B");
+
+	print $graph->compile();
+}
+else
+{
+	header("HTTP/1.1 400 Bad Request");
+	exit(1);
 }
