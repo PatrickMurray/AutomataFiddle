f5aa82b8b01706c43f01cf63bf06ab403f4d20b9
Patrick Murray <patrickm129@gmail.com>
2016-03-19 14:49:41 -0400

    Readded several response headers.

--- src/api/index.php
+++ src/api/index.php
@@ -109,7 +109,10 @@ function json_response($response)
 {
 	/* Handle Cross Site Requests */
 	header("Access-Control-Allow-Origin: *");
-	
+	header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
+	header("Access-Control-Max-Age: 1000");
+	header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
 	header('Content-Type: application/json');
 	
 	if ($_GET["callback"])
