0ba467213facf561d6d7dc8b2aac5077d45a46af
Patrick Murray <patrickm129@gmail.com>
2016-03-21 14:45:40 -0400

    Attempting to add support for origin-destination transitions with different labels.

--- src/api/index.php
+++ src/api/index.php
@@ -494,6 +494,7 @@ function valid_request($request)
 		
 		$origin      = $edge["origin"];
 		$destination = $edge["destination"];
+		$label       = $edge["label"];
 		
 		/* If the origin or destination nodes are undefined */
 		if (!in_array($origin,      $names) ||
@@ -510,13 +511,13 @@ function valid_request($request)
 		}
 		
 		/* Check for duplicate edges */
-		if (in_array($destination, $map[$origin]))
+		if (in_array(array($destination => $label), $map[$origin]))
 		{
 			return False;
 		}
 		
 		/* Add the valid, origin-destination pair to the map */
-		array_push($map[$origin], $destination);
+		array_push($map[$origin], array($destination => $label));
 	}
 	
 	return True;
