206bc621b1454b8232bec2d52cf2e96070a7f84a
Patrick Murray <patrickm129@gmail.com>
2016-07-09 19:38:47 -0400

    reordering web server setup file so that certbot automatically detects the ServerName

--- setup/web_server.sh
+++ setup/web_server.sh
@@ -22,30 +22,6 @@ apt-get install sudo fail2ban apache2 php5 libapache2-mod-php5 php5-mysql php5-m
 # Stop the Apache2 service before we begin modifying configuration files
 service apache2 stop
 
-# Install Let Encrypt and CertBot
-apt-get install python-certbot-apache -t jessie-backports -y
-
-# Get started with CertBot
-certbot --apache certonly
-
-# Create systemd service and timer files
-if [ -f /etc/systemd/system/automatafiddle-ssl-renew.timer ] ; then
-	rm /etc/systemd/system/automatafiddle-ssl-renew.timer;
-fi
-
-if [ -f /etc/systemd/system/automatafiddle-ssl-renew.service ] ; then
-	rm /etc/systemd/system/automatafiddle-ssl-renew.service;
-fi
-
-ln -s $USER_HOME/AutomataFiddle/config/systemd/automatafiddle-ssl-renew.timer /etc/systemd/system/automatafiddle-ssl-renew.timer
-ln -s $USER_HOME/AutomataFiddle/config/systemd/automatafiddle-ssl-renew.service /etc/systemd/system/automatafiddle-ssl-renew.service
-
-chmod +x $USER_HOME/AutomataFiddle/services/automatafiddle-ssl-renew.sh
-
-# Enabling the auto-renew service
-systemctl start automatafiddle-ssl-renew.timer
-systemctl enable automatafiddle-ssl-renew.timer
-
 # Grant www-data permission to use Git and modify the Apache service
 echo "git        ALL = ($USER_NAME) /usr/bin/git"         >> /etc/sudoers
 echo "$USER_NAME ALL = NOPASSWD:     /etc/init.d/apache2" >> /etc/sudoers
@@ -81,5 +57,32 @@ ln -s $USER_HOME/AutomataFiddle/config/apache2/automatafiddle.conf /etc/apache2/
 
 a2enmod rewrite
 a2enmod ssl
+
+
+
+# Install Let Encrypt and CertBot
+apt-get install python-certbot-apache -t jessie-backports -y
+
+# Get started with CertBot
+certbot --apache certonly
+
+# Create systemd service and timer files
+if [ -f /etc/systemd/system/automatafiddle-ssl-renew.timer ] ; then
+	rm /etc/systemd/system/automatafiddle-ssl-renew.timer;
+fi
+
+if [ -f /etc/systemd/system/automatafiddle-ssl-renew.service ] ; then
+	rm /etc/systemd/system/automatafiddle-ssl-renew.service;
+fi
+
+ln -s $USER_HOME/AutomataFiddle/config/systemd/automatafiddle-ssl-renew.timer /etc/systemd/system/automatafiddle-ssl-renew.timer
+ln -s $USER_HOME/AutomataFiddle/config/systemd/automatafiddle-ssl-renew.service /etc/systemd/system/automatafiddle-ssl-renew.service
+
+chmod +x $USER_HOME/AutomataFiddle/services/automatafiddle-ssl-renew.sh
+
+# Enabling the auto-renew service
+systemctl start automatafiddle-ssl-renew.timer
+systemctl enable automatafiddle-ssl-renew.timer
+
 
 service apache2 start
