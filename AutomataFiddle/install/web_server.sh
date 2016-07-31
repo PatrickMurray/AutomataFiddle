56d9c8c06a7d178cc81d23850971046930bd4785
Patrick Murray <patrickm129@gmail.com>
2016-07-30 20:24:18 -0400

    Updating install script regarding certbot and apache configs

--- AutomataFiddle/install/web_server.sh
+++ AutomataFiddle/install/web_server.sh
@@ -36,7 +36,7 @@ fi
 
 
 # Install Fail2Ban, Apache, PHP, MySQL Interface, Git, and GraphViz
-apt-get -y install --reinstall sudo fail2ban apache2 php5 libapache2-mod-php5 php5-mysql php5-mcrypt php5-apcu git graphviz
+apt-get --reinstall install -y sudo fail2ban apache2 php5 libapache2-mod-php5 php5-mysql php5-mcrypt php5-apcu git graphviz
 if [ $? -lt 0 ] ; then
 	echo "Failed to install core packages!"
 	exit -1
@@ -51,7 +51,7 @@ fi
 
 
 # Install Let Encrypt and CertBot
-apt-get install -y -t --reinstall jessie-backports python-certbot-apache
+apt-get --reinstall install -y -t jessie-backports python-certbot-apache
 if [ $? -lt 0 ] ; then
 	echo "Failed to install certbot from the backports!"
 	exit -1
